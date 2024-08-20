32303b4e41218f631aee9b3ba014a3f2becc7591
skywiliancode <codewilian852@gmail.com>
2024-08-20 15:32:55 -0300

    Alterando a função

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+const p = require('./services/products')
+
 async function main() {
-  console.log('Hello system!')
+  console.log(`Carrinho de compras:`)
+  p.getFullName('4010', 'mouse')
+  p.getFullName('3022', 'teclado')
 }
 
 
